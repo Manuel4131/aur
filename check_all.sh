55421deb052601aa8f28666531ced1726247a9ef
Yen Chi Hsuan <yan12125@gmail.com>
2015-03-07 18:59:13 +0800

    Add pypy-pip check script

--- check_all.sh
+++ check_all.sh
@@ -9,3 +9,7 @@ source ./spim-svn/PKGBUILD
 echo PKGBUILD version = $pkgver
 echo Upstream version = `php ./spim-svn/spim-svn.php`
 
+echo pypy-pip
+source ./pypy-pip/PKGBUILD
+echo PKGBUILD version = $pkgver
+echo Upstream version = `python ./pypy-pip/ver.py`
